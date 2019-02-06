754dfe039b2600d56701f1fef5bdc5f9a8da03f0
Richard Walsh <richard@zoocreative.net>
2019-02-06 21:23:25 +0000

    Added Basic Dragging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dragndrop';
+
+  numbers: number[] = [];
+
+  constructor() {
+    for (let i = 0; i<1000; i++) {
+      this.numbers.push(i); 
+    }
+  }
 }
